69bea52064a8d10982e3312fd9603e895a756fe9
israelitalo <israelitalo2012@gmail.com>
2020-05-15 11:58:07 -0300

    Arquivo do banco de dados atualizado

--- banco de dados/casadospobres.sql
+++ banco de dados/casadospobres.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 13-Maio-2020 às 17:56
+-- Tempo de geração: 15-Maio-2020 às 16:57
 -- Versão do servidor: 10.4.11-MariaDB
 -- versão do PHP: 7.4.3
 
@@ -21,53 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `casadospobres`
 --
-CREATE DATABASE IF NOT EXISTS `casadospobres` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `casadospobres`;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuario`
---
-
-CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(200) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `senha` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `usuario`
---
-
-INSERT INTO `usuario` (`id`, `nome`, `email`, `senha`) VALUES
-(1, 'israel italo bernardo cabral silva', 'israelitalo2012@gmail.com', '$2y$10$00aRgOrhT2kHIrvOJdUSvu.e21q9ls0i6sQN4iA4g/AXpjDANmDA6'),
-(2, 'nataly costa mendes', 'natalymendes@gmail.com', '$2y$10$fhWSc0UtN.DOCaetMHlCJe8XSBPWj8NYdmYG7zHR9jCQDOt7ufygq'),
-(3, 'fatima cabral', 'fatimacabral@gmail.com', '$2y$10$eWbfRl5dBHo0QtWGci2cxOPsg8c.7KIHu/pkoobkq6uY4tL3kKT6W'),
-(4, 'isaias joaquim', 'isaiasjoaquim@gmail.com', '$2y$10$QgEMQ27CujSZPwdT8z5iwOXZhSkfxYxYk0o0k2OI2oWo1BULc4Haq'),
-(7, 'teste excluir', 'testeexcluir@gmail.com', '$2y$10$WFYtXidkeHwC0tLkBeXO0uQm.faY7dTHc3EiVzKcb7c440Ed6LHSy');
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices para tabela `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
